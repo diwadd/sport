987f1144b2053b7652197e39b13534c238249a5f
Dawid Dul <dawid.dul@gmail.com>
2019-11-23 22:24:11 +0100

    Correct solution for Pinary Palindromes

--- cf_binary_palindromes.py
+++ cf_binary_palindromes.py
@@ -2,64 +2,29 @@ import math
 
 def make_bs(s_list):
 
-    t_zeros = 0
-    t_ones = 0
     n = len(s_list)
-    n_zeros_in_strings = [0 for i in range(n)]
-    n_ones_in_strings = [0 for i in range(n)]
+    res = n
+    n_odd_strings = 0
+    n_even_bad_strings = 0
     for i in range(n):
         s = s_list[i]
         m = len(s)
-        for j in range(m):
-            if s[j] == "0":
-                n_zeros_in_strings[i] = n_zeros_in_strings[i] + 1
-                t_zeros = t_zeros + 1
-            if s[j] == "1":
-                n_ones_in_strings[i] = n_ones_in_strings[i] + 1
-                t_ones = t_ones + 1
-    
-    n_even_zero_strings = 0
-    n_odd_zero_strings = 0
-    n_even_one_strings = 0
-    n_odd_one_strings = 0
+        if m % 2 == 1:
+            n_odd_strings += 1
+        else:
+            n_zeros = s.count("0")
+            n_ones = s.count("1")
+            if n_zeros % 2 == 1 or n_ones % 2 == 1:
+                n_even_bad_strings += 1
 
-    res = 0
-    for i in range(n):
-        s = s_list[i]
-        m = len(s)
-        print(f"m: {m} s: {s} t_zeros: {t_zeros} t_ones: {t_ones} n_odd_zero_strings: {n_odd_zero_strings} n_odd_one_strings: {n_odd_one_strings}")
-        if m <= t_zeros:
-            t_zeros = t_zeros - m
-            if m % 2 == 0:
-                n_even_zero_strings = n_even_zero_strings + 1
-            else:
-                n_odd_zero_strings = n_odd_zero_strings + 1
-            res = res + 1
-            continue
-        if m <= t_ones:
-            t_ones = t_ones - m
-            if m % 2 == 0:
-                n_even_one_strings = n_even_one_strings + 1
-            else:
-                n_odd_one_strings = n_odd_one_strings + 1
-            res = res + 1
-            continue
-
-        if m <= t_zeros + t_ones:
-            if t_zeros > t_ones:
-                t_ones = t_ones - (m - t_zeros)
-                t_zeros = 0
-                res = res + 1
-            elif t_ones > t_zeros:
-                t_zeros = t_zeros - (m - t_ones)
-                t_ones = 0
-                res = res + 1
+    if n_odd_strings == 0 and n_even_bad_strings % 2 == 1:
+        res -= 1
 
     return res
 
 
 if __name__ == "__main__":
-    i_mode = False
+    i_mode = True
 
     if i_mode:
         q = input()
