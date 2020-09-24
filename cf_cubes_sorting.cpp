ad5e1afe55a1d269d2baadc66275b3fd0cdc8572
Dawid Dul <dawid.dul@gmail.com>
2020-09-25 00:03:38 +0200

    Cubes and max sort operations needed

---
+++ cf_cubes_sorting.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+typedef long long int lli;
+typedef unsigned long long int ulli;
+typedef long double ld;
+
+void left_just(string &s, int n, char c = ' ') {
+    while(s.length() < n) {
+        s = c + s;
+    }
+}
+
+template<typename T> void print_vector(vector<T> &vec) {
+
+    int n = vec.size();
+    for(int i = 0; i < n; i++) {
+        if(i == n - 1)
+            cout << vec[i];
+        else
+            cout << vec[i] << " ";
+    }
+    cout << "\n";
+}
+
+template<typename T> void print_matrix(vector<vector<T>> &mat) {
+    for(int i = 0; i < mat.size(); i++) {
+        print_vector(mat[i]);
+    }
+}
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    for(int t = 0; t < T; t++) {
+
+        int N;
+        cin >> N;
+
+        vector<int> a_vec(N, 0);
+        for(int n = 0; n < N; n++) {
+            cin >> a_vec[n];
+        }
+
+        bool is_ok = true;
+        for(int i = 1; i < N; i++) {
+            // cout << "a_vec[i]: " << a_vec[i] << " a_vec[i-1]: " << a_vec[i-1] << endl;
+            if(a_vec[i] < a_vec[i-1]) {
+                continue;
+            } else {
+                is_ok = false;
+                break;
+            }
+        }
+
+
+        // cout << "is_ok: " << is_ok << endl;
+        if(is_ok == true) {
+            cout << "NO\n";
+        } else {
+            cout << "YES\n";
+        }
+
+    }
+
+}
