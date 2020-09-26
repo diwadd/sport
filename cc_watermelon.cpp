49a2ab2595b62b0bfedf6d66ac7fbdd9f3c453aa
Dawid Dul <dawid.dul@gmail.com>
2020-09-26 22:43:09 +0200

    September Lunchtime 2020 - Watermelon

---
+++ cc_watermelon.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+typedef long long int lli;
+typedef unsigned long long int ulli;
+typedef long double ld;
+
+void left_just(string &s, int n, char c = ' ') {
+    while(s.length() < n) {
+        s = c + s;
+    }
+}
+
+template<typename T> void print_vector(vector<T> &vec) {
+
+    int n = vec.size();
+    for(int i = 0; i < n; i++) {
+        if(i == n - 1)
+            cout << vec[i];
+        else
+            cout << vec[i] << " ";
+    }
+    cout << "\n";
+}
+
+template<typename T> void print_matrix(vector<vector<T>> &mat) {
+    for(int i = 0; i < mat.size(); i++) {
+        print_vector(mat[i]);
+    }
+}
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+ 
+    for(int t = 0; t < T; t++) {
+ 
+        int N;
+        cin >> N;
+
+        lli sum = 0;
+        for(int i = 0; i < N; i++) {
+          lli a;
+          cin >> a;
+          sum += a;
+        }
+
+        // cout << "sum: " << sum << "\n";
+        if (sum < 0) {
+          cout << "NO\n";
+        } else if(sum >= 0) {
+          cout << "YES\n";
+        }
+
+    }
+}
