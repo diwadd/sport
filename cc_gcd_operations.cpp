62d352cdcdbdd015646533dbef625fc69e47e948
Dawid Dul <dawid.dul@gmail.com>
2020-09-27 22:50:22 +0200

    September Lunchtime 2020 - GCD Operations

---
+++ cc_gcd_operations.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+typedef long long int lli;
+typedef unsigned long long int ulli;
+typedef long double ld;
+
+void left_just(string &s, int n, char c = ' ') {
+    while(s.length() < n) {
+        s = c + s;
+    }
+}
+
+template<typename T> void print_vector(vector<T> &vec) {
+
+    int n = vec.size();
+    for(int i = 0; i < n; i++) {
+        if(i == n - 1)
+            cout << vec[i];
+        else
+            cout << vec[i] << " ";
+    }
+    cout << "\n";
+}
+
+template<typename T> void print_matrix(vector<vector<T>> &mat) {
+    for(int i = 0; i < mat.size(); i++) {
+        print_vector(mat[i]);
+    }
+}
+
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    for(int t = 0; t < T; t++) {
+
+        lli N;
+        cin >> N;
+
+        bool is_ok = true;
+        for(lli n = 0; n < N; n++) {
+            lli b;
+            cin >> b;
+
+            if((n+1) % b != 0)
+                is_ok = false;
+        }
+
+        if(is_ok == true) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+
+    }
+
+}
