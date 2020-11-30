dc6a88e367e63a9500535b4cfd7e1cb880d91fdb
Dawid Dul <dawid.dul@gmail.com>
2020-11-30 21:55:37 +0100

    Strange functions

---
+++ cf_strange_functions.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+typedef long long int lli;
+typedef unsigned long long int ulli;
+typedef long double ld;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+ 
+    for(int t = 0; t < T; t++) {
+ 
+        string s;
+        cin >> s;
+
+        cout << s.size() << "\n";
+
+    }
+
+
+}
