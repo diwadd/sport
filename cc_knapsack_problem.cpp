2fe376cd52c763934604850c72e55e526de9d1f6
Dawid Dul <dawid.dul@gmail.com>
2021-02-23 23:30:39 +0100

    Top down approach - still TLE

--- cc_knapsack_problem.cpp
+++ cc_knapsack_problem.cpp
@@ -17,24 +17,31 @@ template<typename T> void print_vector(vector<T> &vec) {
     cout << "\n";
 }
 
-ulli knapsack(int n, int capacity, vector<int> &w_vec, vector<int> &c_vec, vector<vector<ulli>> &dp) {
 
-    if (n == -1 or capacity == 0) {
-        return 0;
-    } else if (dp[n][capacity] != -1) {
-        return dp[n][capacity];
-    } else if (w_vec[n] > capacity) {
-        if(dp[n][capacity] != -1)
-            return dp[n-1][capacity];
-        else
-            return knapsack(n - 1, capacity, w_vec, c_vec, dp);
-    } else {
-        ulli t1 = knapsack(n - 1, capacity, w_vec, c_vec, dp);
-        ulli t2 = c_vec[n] + knapsack(n - 1, capacity - w_vec[n], w_vec, c_vec, dp);
-        dp[n][capacity] = max(t1, t2);
-        return dp[n][capacity];
+template<typename T> void print_matrix(vector<vector<T>> &mat) {
+    for(int i = 0; i < mat.size(); i++) {
+        print_vector(mat[i]);
     }
 }
+
+// ulli knapsack(int n, int capacity, vector<int> &w_vec, vector<int> &c_vec, vector<vector<ulli>> &dp) {
+
+//     if (n == -1 or capacity == 0) {
+//         return 0;
+//     } else if (dp[n][capacity] != -1) {
+//         return dp[n][capacity];
+//     } else if (w_vec[n] > capacity) {
+//         if(dp[n][capacity] != -1)
+//             return dp[n-1][capacity];
+//         else
+//             return knapsack(n - 1, capacity, w_vec, c_vec, dp);
+//     } else {
+//         ulli t1 = knapsack(n - 1, capacity, w_vec, c_vec, dp);
+//         ulli t2 = c_vec[n] + knapsack(n - 1, capacity - w_vec[n], w_vec, c_vec, dp);
+//         dp[n][capacity] = max(t1, t2);
+//         return dp[n][capacity];
+//     }
+// }
 
 int main() {
 
@@ -58,12 +65,36 @@ int main() {
     }
 
     vector<ulli> res(M, 0);
-    vector<vector<ulli>> dp(N, vector<ulli>(M+1, -1));
-    for(int m = 1; m <= M; m++) {
-        // cout << "m: " << m << endl;
-        res[m-1] = knapsack(N-1, m, w_vec, c_vec, dp);
+    vector<vector<ulli>> dp(N+1, vector<ulli>(M+2, numeric_limits<ulli>::max()));
+ 
+    for(int i = N; i >= 0; i--) {
+        for(int j = 0; j <= M+1; j++) {
 
-    }    
+            if(i == N) {
+                dp[i][j] = 0;
+            } else {
+
+                ulli t1 = dp[i+1][j];
+                ulli t2 = 0;
+                if(j > w_vec[i]){
+                    t2 = dp[i+1][j - w_vec[i]] + c_vec[i];
+                }
+                dp[i][j] = max(t1, t2);
+            }
+        }
+    }
+ 
+    for(int m = 1; m <= M+2; m++) {
+        res[m-1] = dp[0][m+1];
+    }
+
+    // print_matrix(dp);
+
+    // for(int m = 1; m <= M; m++) {
+    //     // cout << "m: " << m << endl;
+    //     res[m-1] = knapsack(N-1, m, w_vec, c_vec, dp);
+
+    // }    
 
     print_vector(res);
 
